3191a6ca555f95ac8feb3f6d77c4783f3024c145
Phuong <phuong.dao@nabstudio.com>
2021-02-10 09:39:17 +0700

    update server & db

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./services/database');
-const port = process.env.PORT || 3030;
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./services/database");
+const passport = require("passport");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.static("public"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// // middlewares
+// app.use(passport.initialize());
+// require("./middlewares/passport")(passport);
+// app.get(
+//   "/",
+//   passport.authenticate("jwt", { session: false }),
+//   async (req, res) => {
+//     res.json({
+//       message: "Okay.",
+//     });
+//   }
+// );
 
-// app.use("/register", require("./routes/register"));
-// app.use("/login", require("./routes/login"));
-// app.use("/user", require("./routes/user"));
-app.use("/flight", require("./routes/flight"));
+app.get("/", (req, res) => {
+  res.send("HI GUY");
+});
+
+app.use("/register", require("./routes/register"));
+app.use("/login", require("./routes/login"));
+app.use("/user", require("./routes/user"));
 app.use("/airport", require("./routes/airport"));
+app.use("/flight", require("./routes/flight"));
+app.use("/middle-airport", require("./services/middle-airport"));
+app.use("/history-sale", require("./services/history-sale"));
 
 db.sync()
   .then(() => {
-    app.listen(port);
-    console.log("server running at : ", port);
-    console.log(`http://localhost:${port}/`);
+    app.listen(PORT);
+    console.log("🚀 Server on. PORT : ", PORT);
   })
   .catch((err) => {
-    console.log(err);
+    console.log("❌ Setup db failed: " + err);
+    console.error(err);
   });
