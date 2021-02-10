999a3a85ee379bbe0701534e9c45f8bc97bd1245
Phuong <phuong.dao@nabstudio.com>
2021-02-10 21:51:17 +0700

    fix lint

--- models/history-sale.js
+++ models/history-sale.js
@@ -1,52 +1,42 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 const Model = Sequelize.Model;
 const User = require('./user');
 const Flight = require('./flight');
 
 class HistorySale extends Model {
-    static createHistorySale = async ({
-        userID,
-        flightCode,
-        typeSeat,
-        dateSale,
-        status
-    }) => {
-        return await HistorySale.create({
-            userID,
-            flightCode,
-            typeSeat,
-            dateSale,
-            status
-        });
-    };
+  static createHistorySale = async ({ userID, flightCode, typeSeat, dateSale, status }) => {
+    return await HistorySale.create({
+      userID,
+      flightCode,
+      typeSeat,
+      dateSale,
+      status,
+    });
+  };
 
-    static updateHistorySale = async ({
-    }) => {
-        return await HistorySale.update({
+  static updateHistorySale = async ({}) => {
+    return await HistorySale.update(
+      {},
+      {
+        where: {},
+      }
+    );
+  };
 
-        },
-            {
-                where: {}
-            });
-    };
+  static async getAllSale() {
+    return HistorySale.findAll();
+  }
 
-    static async getAllSale() {
-        return HistorySale.findAll();
-    };
+  static async getHistorySaleByUser(userID) {
+    return await HistorySale.findOne({
+      where: { userID: userID },
+    });
+  }
 
-    static async getHistorySaleByUser(userID) {
-        return await HistorySale.findOne({
-            where: { userID: userID },
-        });
-    };
-
-    static async getHistorySaleByYear(year) {
-        return await HistorySale.findAll({ 
-           
-        });
-    };
-
+  static async getHistorySaleByYear(year) {
+    return await HistorySale.findAll({});
+  }
 }
 
 HistorySale.init(
@@ -73,14 +63,13 @@ HistorySale.init(
       allowNull: false,
     },
 
-        dateSale: {
-            type: Sequelize.DATE,
-            allowNull: false,
-        },
-        status: {
-            type: Sequelize.BOOLEAN,
-            allowNull: true,
-        }
+    dateSale: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.BOOLEAN,
+      allowNull: true,
     },
   },
   {
