af193c6d24670b73ffa0bce8328749fb76f9b817
Phuong <phuong.dao@nabstudio.com>
2021-02-10 20:01:48 +0700

    update api user

--- routes/user.js
+++ routes/user.js
@@ -56,7 +56,7 @@ router.post('/register', function (req, res, next) {
     });
 });
 
-router.post('/changePassword', passport.authenticate('jwt', { session: false }), async (req, res) => {
+router.post('/change-password', passport.authenticate('jwt', { session: false }), async (req, res) => {
   const userId = _.get(req, 'user.dataValues.id', null);
   const oldPass = _.get(req, 'body.oldPass', null);
   const newPass = _.get(req, 'body.newPass', null);
@@ -73,25 +73,6 @@ router.post('/changePassword', passport.authenticate('jwt', { session: false }),
     message: 'Successfully updated password.',
   });
 });
-
-// router.post('/forgotPassword', async (req, res) => {
-//   const user = await User.getUser({
-//     email,
-//   });
-
-//   if (!user) {
-//     return res.status(400).send({
-//       error: 'Email not exist.',
-//     });
-//   }
-//   let rPass = Math.random().toString(36).substring(3);
-//   // update new password
-//   // await User.updatePassword(user.id, rPass);
-//   await sendMail(email, '[] - Password reset', `Your username is : ${user.username}`);
-//   res.json({
-//     message: 'Okay',
-//   });
-// });
 
 router.post('/forgot-password', async (req, res) => {
   const email = _.get(req, 'body.email', null);
@@ -127,29 +108,31 @@ router.post('/forgot-password', async (req, res) => {
 router.put('/update-user', passport.authenticate('jwt', { session: false }), async (req, res) => {
   // check staff token
   const stateUser = _.get(req, 'user.dataValues');
-  if (stateUser.role !== 'staff') {
-    return res.status(400).send({
-      error: 'Staff required.',
+  if (stateUser.role !== 'ADMIN') {
+    return res.status(403).send({
+      error: 'Forbidden',
     });
   }
-  // get userId to update
-  const userId = _.get(req, 'query.userId', null);
+
   const user = await User.getUser({
-    id: userId,
+    id: stateUser.id,
   });
+
   if (!user) {
     return res.status(400).send({
       error: 'User not found.',
     });
   }
+
   const data = _.get(req, 'body');
+
   try {
     await User.updateUser({
       userId,
       ...data,
     });
     return res.json({
-      message: 'Sucessfully updated user.',
+      message: 'Success updated user.',
     });
   } catch (err) {
     console.log(err);
